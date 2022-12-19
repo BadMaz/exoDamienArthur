850fcdb9e1310ac273272c618ea1c76b1d2fd57b
Damien POTTIER <damiendevp@gmail.com>
2022-12-19 17:19:13 +0100

    Modification du deuxieme SYSO

--- exoGitHub/main.java
+++ exoGitHub/main.java
@@ -4,7 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		//Creation du main
 		System.out.println("Coucou c'est nous!");
-		System.out.println("Hello world");
+		System.out.println("BYE world!");
 	}
 
 }
